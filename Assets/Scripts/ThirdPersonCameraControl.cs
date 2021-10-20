e790f7e091ec3928cb6f2307b9091c088ff9f544
Aki <shibata.aki.wainwright@gmail.com>
2021-10-20 05:07:26 +0100

    Messed around with 3rd person camera

--- Assets/Scripts/ThirdPersonCameraControl.cs
+++ Assets/Scripts/ThirdPersonCameraControl.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class ThirdPersonCameraControl : MonoBehaviour
 {
     public float cameraSensitivity = 3f;
-    public bool inverted;
-    private float m_verticalRot;
-    
+    public bool inverted = true;
+
+
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        m_verticalRot = 0;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+
         transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraSensitivity, Space.World);
 
-        if (inverted)
-        {
-            transform.Rotate(Vector3.right, -Input.GetAxis("Mouse Y") * cameraSensitivity, Space.Self);
-        }
-        else
+        /*if (inverted)
         {
             transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y") * cameraSensitivity, Space.Self);
         }
+        else
+        {
+            transform.Rotate(Vector3.right, -Input.GetAxis("Mouse Y") * cameraSensitivity, Space.Self);
+        }*/
     }
 }
