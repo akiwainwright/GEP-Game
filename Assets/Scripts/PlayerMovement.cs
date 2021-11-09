b30e3818deeb2e654748e9fa02f1f5a93c90f328
Aki <akiwainwright@gmail.com>
2021-11-09 15:04:11 +0000

    Improved movement code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform playerCamera;
     public float moveSpeed = 5f;
-    public GameObject camera;
     [Range(0,1)]public float turnSpeed = 0.5f;
     
     private Rigidbody m_RB;
@@ -18,58 +18,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        #region Movement
-        //Movement split up as horizontal behaviour will be different to vertical movement
-        //Combination of vertical and horizontal movement will just combine the result of both 
-
-        #region Combined Direction
-        //If player is moving in a combination of a vertical and horizontal direction
-        if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") != 0)
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            Vector3 bothMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            bothMovement.Normalize();
-            Quaternion facingRotation = Quaternion.LookRotation(bothMovement, Vector3.up);
-            m_RB.velocity = bothMovement * moveSpeed;
+            //Moving based on camera dimensions
+            Vector3 horizontalMovement = playerCamera.right * Input.GetAxis("Horizontal");
+            Vector3 verticalMovement = Vector3.ProjectOnPlane(playerCamera.forward, Vector3.up) * Input.GetAxis("Vertical");
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, facingRotation, turnSpeed);
+            Vector3 totalMovement = horizontalMovement + verticalMovement;
+            totalMovement.Normalize();
+
+            m_RB.velocity = totalMovement * moveSpeed * Time.fixedDeltaTime;
+
+            m_RB.transform.forward = totalMovement;
         }
-        #endregion
-
-        #region Forwards and Backwards
-        //If player is moving in a vertical direction i.e forwards or backwards
-        if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") == 0)
-        {
-            Vector3 verticalMovement = new Vector3(0, 0, Input.GetAxis("Vertical"));
-            verticalMovement.Normalize();
-
-            Quaternion facingRotation = Quaternion.LookRotation(verticalMovement, Vector3.up);
-            m_RB.velocity = verticalMovement * moveSpeed;
-
-            m_RB.velocity = verticalMovement * moveSpeed;
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, facingRotation, turnSpeed);
-        }
-        #endregion
-
-        #region Left and Right
-        //If player is moving in a horizontal direction i.e left or right
-        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") != 0)
-        {
-            
-            Vector3 horizontalMovement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            horizontalMovement.Normalize();
-
-            Quaternion facingRotation = Quaternion.LookRotation(horizontalMovement, Vector3.up);
-            m_RB.velocity = horizontalMovement * moveSpeed;
-
-            m_RB.velocity = horizontalMovement * moveSpeed;
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, facingRotation, turnSpeed);
-
-        }
-        #endregion
-
-        #endregion
-
     }
 }
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -5,35 +5,24 @@ using UnityEngine;
 public class ThirdPersonCamera : MonoBehaviour
 {
     public GameObject target;
-    public Vector3 cameraOffset;
+
+    public float distanceFromPlayer;
 
     public float cameraSensitivity = 3f;
     
     [Range(0, 1)]public float smooth = 0.1f;
 
     private float m_Smoother;
-    
 
-
-    private Vector3 m_velocity = Vector3.zero;
-
-    private float m_rotationX;
-    private float m_rotationY;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
-        //using exponential smoothing to get smoother result
-        m_Smoother = 1f - Mathf.Pow(smooth, Time.deltaTime);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 destination = target.transform.position + cameraOffset;
-        
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, destination, m_Smoother);
-        
+        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);        
     }
 }
